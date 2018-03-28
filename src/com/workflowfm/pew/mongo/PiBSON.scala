499892a576303e54cdee81ff5f8baecd60fe3035
Petros Papapanagiotou <pe.p@ed.ac.uk>
2018-03-28 15:32:38 +0000

    Pew: Created a working PiItem codec. Improved testing, borrowing methods from https://github.com/mongodb/mongo-scala-driver/blob/master/bson/src/test/scala/org/mongodb/scala/bson/codecs/MacrosSpec.scala I now have a better understanding of codecs and BSON encoding. These helped: https://stackoverflow.com/questions/30569228/mongodb-bson-codec-not-being-used-while-encoding-object/30655264 https://stackoverflow.com/questions/45083885/decode-document-to-a-java-class-using-custom-mongo-codec
    git-svn-id: https://svn.ecdf.ed.ac.uk/repo/inf/Hol_Light/services/dev/pap/pew@1904 905188e7-2b63-485d-9a76-7c6572d3f986

--- src/com/workflowfm/pew/mongo/PiBSON.scala
+++ src/com/workflowfm/pew/mongo/PiBSON.scala
@@ -14,27 +14,64 @@ import org.bson.codecs.configuration.CodecRegistry
 import org.bson.codecs.configuration.CodecRegistries
 
 
-class PiItemCodec extends Codec[PiItem[_]] {
-  override def encode(writer: BsonWriter, value: PiItem[_], encoderContext: EncoderContext): Unit = value match {
+class PiItemCodec extends Codec[PiItem[_]] {  
+  override def encode(writer: BsonWriter, value: PiItem[_], encoderContext: EncoderContext): Unit = {
+    writer.writeStartDocument()
+    writer.writeName("piitem")
+    value match {
+    /*case PiItem(v: String) => writer.writeString(wrap(v))
+    case PiItem(v: Int) => writer.writeString(wrap(v.toString()))
+    case PiItem(v: Long) => writer.writeString(wrap(v.toString()))
+    case PiItem(v: Boolean) => writer.writeString(wrap(v.toString()))
+    case PiItem(v: ObjectId) => writer.writeString(wrap(v.toString()))*/
     case PiItem(v: String) => writer.writeString(v)
     case PiItem(v: Int) => writer.writeInt32(v)
     case PiItem(v: Long) => writer.writeInt64(v)
     case PiItem(v: Boolean) => writer.writeBoolean(v)
-    case PiItem(v: ObjectId) => writer.writeObjectId(v)
-  }
+    case PiItem(v: ObjectId) => writer.writeObjectId(v) 
+    
+   }
+   writer.writeEndDocument() }
 
   override def getEncoderClass: Class[PiItem[_]] = classOf[PiItem[_]]
 
   override def decode(reader: BsonReader, decoderContext: DecoderContext): PiItem[_] = {
-    reader.getCurrentBsonType match {
+    reader.readStartDocument()
+    reader.readName()
+    val ret = reader.getCurrentBsonType match {
       case BsonType.BOOLEAN =>PiItem(reader.readBoolean())
       case BsonType.STRING => PiItem(reader.readString())
       case BsonType.INT64 => PiItem(reader.readInt64())
       case BsonType.INT32 => PiItem(reader.readInt32())
       case BsonType.OBJECT_ID => PiItem(reader.readObjectId())
     }
+    reader.readEndDocument()
+    ret
   }
 }
+//org.bson.BsonInvalidOperationException: writeString can only be called when State is VALUE, not when State is NAME
+	
+//class PiObjectCodec(registry:CodecRegistry) extends Codec[PiObject] {
+//    override def encode(writer:BsonWriter, obj:PiObject, encoderContext:EncoderContext) = {
+//        writeMap(writer, document, encoderContext);
+//    }
+//
+//    override def decode(reader:BsonReader, decoderContext:DecoderContext) = {
+//        Document document = new Document();
+//
+//        reader.readStartDocument();
+//        while (reader.readBsonType() != BsonType.END_OF_DOCUMENT) {
+//            String fieldName = reader.readName();
+//            document.put(fieldName, readValue(reader, decoderContext));
+//        }
+//
+//        reader.readEndDocument();
+//
+//        return document;
+//    }
+//
+//    override def getEncoderClass() = classOf[PiObject]
+//}
 
 class PiObjectCodecProvider extends CodecProvider {
   val chanCodecProvider = Macros.createCodecProvider[Chan]()
--- test/com/workflowfm/pew/mongo/PiBSONTests.scala
+++ test/com/workflowfm/pew/mongo/PiBSONTests.scala
@@ -1,9 +1,9 @@
 package com.workflowfm.pew.mongo
 
-import org.mongodb.scala.bson.codecs._
+import org.mongodb.scala.bson.collection.immutable.Document
+import org.bson.codecs.configuration.CodecRegistries.{fromRegistries, fromProviders, fromCodecs}
 import org.mongodb.scala.bson.codecs.Macros
 import org.mongodb.scala.bson.codecs.DEFAULT_CODEC_REGISTRY
-import org.bson.codecs.configuration.CodecRegistries._
 import com.workflowfm.pew._
 import org.bson.types._
 import org.bson._
@@ -11,15 +11,23 @@ import org.bson.codecs._
 import org.bson.codecs.configuration.CodecProvider
 import org.bson.codecs.configuration.CodecRegistry
 import org.bson.codecs.configuration.CodecRegistries
+
+
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.bson.io._
+
 import java.nio.ByteBuffer
+import java.util
+
+import scala.collection.JavaConverters._
+import scala.language.implicitConversions
+import scala.reflect.ClassTag
 
 @RunWith(classOf[JUnitRunner])
-class PiBSONTests extends FlatSpec with Matchers {
+class PiBSONTests extends FlatSpec with Matchers with PiBSONTestHelper {
 	
   //val p = new PiObjectCodecProvider
   
@@ -38,21 +46,124 @@ class PiBSONTests extends FlatSpec with Matchers {
 	      fromCodecs(new PiItemCodec),
 	      DEFAULT_CODEC_REGISTRY) 
   
-  //val r = fromRegistries(fromProviders(p), DEFAULT_CODEC_REGISTRY)
+  //val r = fromRegistries(fromProviders(p), DEFAULT_CODEC_REGISTRY
   
-  val obj = PiPair(PiItem("Oh!"),PiItem("Hi!"))
+	"codec" should "encode/decode PiItems" in {
+//	  val obj = PiItem("Oh!")
+//	  
+	  val codec = new PiItemCodec
+//
+//	  val writer: BsonBinaryWriter = new BsonBinaryWriter(new BasicOutputBuffer())
+//	  codec.encode(writer, obj, EncoderContext.builder().build())
+//	  
+//	  val buffer: BasicOutputBuffer = writer.getBsonOutput().asInstanceOf[BasicOutputBuffer];
+//	  val reader: BsonBinaryReader = new BsonBinaryReader(new ByteBufferBsonInput(new ByteBufNIO(ByteBuffer.wrap(buffer.toByteArray))))
+//		val dec = codec.decode(reader, DecoderContext.builder().build())
+//		
+//		obj should be (dec)
+	  
+	  roundTrip(PiItem("Oh!"), """{piitem: "Oh!"}""", codec)
+	  roundTrip(PiItem(9), """{piitem: 9}""", codec)
+  }     
   
-  val writer: BsonBinaryWriter = new BsonBinaryWriter(new BasicOutputBuffer())
-  reg.get(classOf[PiPair]).encode(writer, obj, EncoderContext.builder().build())
-  System.err.println(">>>" + writer.getBsonOutput().asInstanceOf[BasicOutputBuffer].toByteArray())
+// This doesn't work because the decoder doesn't know how to deal with the (sub-)Document and that it should be mapped to a PiItem.
+// You either (a) manually decode the Document to a PiItem or (b) use a bsonTypeCodecMap to map Documents to PiItems 
   
-  val buffer: BasicOutputBuffer = writer.getBsonOutput().asInstanceOf[BasicOutputBuffer];
-  val reader: BsonBinaryReader = new BsonBinaryReader(new ByteBufferBsonInput(new ByteBufNIO(ByteBuffer.wrap(buffer.toByteArray))))
+//	"codec" should "encode/decode a Document with a PiItem" in {
+//	  val obj = PiItem("Oh!")
+//	  
+//	  val doc = new Document().append("x", obj)
+//	  
+//	   System.err.println(">>> " + doc + " | " + doc.get("x") + ":" + doc.get("x").getClass)
+//	  
+//	  val codec = new PiItemCodec
+//	  val reg = fromRegistries(fromCodecs(codec), DEFAULT_CODEC_REGISTRY) 
+//	  val docc = new DocumentCodec(reg)
+//
+//	  val writer: BsonBinaryWriter = new BsonBinaryWriter(new BasicOutputBuffer())
+//	  docc.encode(writer, doc, EncoderContext.builder().build())
+//
+//	  val buffer: BasicOutputBuffer = writer.getBsonOutput().asInstanceOf[BasicOutputBuffer];
+//	  val reader: BsonBinaryReader = new BsonBinaryReader(new ByteBufferBsonInput(new ByteBufNIO(ByteBuffer.wrap(buffer.toByteArray))))
+//	  val dec = docc.decode(reader, DecoderContext.builder().build())
+//		
+//	  System.err.println(">>>! " + dec + " | " + dec.get("x") + ":" + dec.get("x").getClass)
+//		
+//		doc should be (dec)
+//  }  
+	   
+  
+  "codec" should "decode a custom sealed trait somehow" in {
+    sealed trait Patata
+    case class Ntomata(n:Int) extends Patata
+    case class Mpou(mxs:Int) extends Patata
+    
+    sealed class XTree
+    case class XBranch(b1: XTree, b2: XTree, value: Int) extends XTree
+    case class XLeaf(value: Int) extends XTree
 
-  val dec = reg.get(classOf[PiPair]).decode(reader, DecoderContext.builder().build())
-  System.err.println(">>>! " + dec)
+    //val codecRegistry = fromRegistries( fromProviders(classOf[Tree]), DEFAULT_CODEC_REGISTRY )
+    
+    val p = Macros.createCodecProvider[XTree]()
+    val reg = fromRegistries(fromProviders(p),DEFAULT_CODEC_REGISTRY) 
+    
+    roundTrip(XLeaf(5), """{value: 5}""", p)
+  }
+//  "ha" should "tell the truth" in {
+//	  val obj = PiPair(PiItem("Oh!"),PiItem("Hi!"))
+//
+//			  val writer: BsonBinaryWriter = new BsonBinaryWriter(new BasicOutputBuffer())
+//			  reg.get(classOf[PiPair]).encode(writer, obj, EncoderContext.builder().build())
+//			  System.err.println(">>>" + writer.getBsonOutput().asInstanceOf[BasicOutputBuffer].toByteArray())
+//
+//			  val buffer: BasicOutputBuffer = writer.getBsonOutput().asInstanceOf[BasicOutputBuffer];
+//			  val reader: BsonBinaryReader = new BsonBinaryReader(new ByteBufferBsonInput(new ByteBufNIO(ByteBuffer.wrap(buffer.toByteArray))))
+//
+//					  val dec = reg.get(classOf[PiPair]).decode(reader, DecoderContext.builder().build())
+//					  System.err.println(">>>! " + dec)
+//					  1 should be (1)
+//  }
+}
+
+trait PiBSONTestHelper {
+  def roundTrip[T](value: T, expected: String, provider: CodecProvider, providers: CodecProvider*)(implicit ct: ClassTag[T]): Unit = {
+    val codecProviders: util.List[CodecProvider] = (provider +: providers).asJava
+    val registry = CodecRegistries.fromRegistries(CodecRegistries.fromProviders(codecProviders), DEFAULT_CODEC_REGISTRY)
+    val codec = registry.get(ct.runtimeClass).asInstanceOf[Codec[T]]
+    roundTripCodec(value, Document(expected), codec)
+  }
   
-  "ha" should "tell the truth" in {
-		1 should be (1)
-	}
+  def roundTrip[T](value: T, expected: String, codec: Codec[T]): Unit = {
+    roundTripCodec(value, Document(expected), codec)
+  }
+
+  def roundTripCodec[T](value: T, expected: Document, codec: Codec[T]): Unit = {
+    val encoded = encode(codec, value)
+    val actual = decode(documentCodec, encoded)
+    System.out.println(s"Encoded document: (${actual.toJson()}) must equal: (${expected.toJson()})")
+    assert(expected == actual, s"Encoded document: (${actual.toJson()}) did not equal: (${expected.toJson()})")
+
+    val roundTripped = decode(codec, encode(codec, value))
+    System.out.println(s"Round Tripped case class: ($roundTripped) must equal the original: ($value)")
+    assert(roundTripped == value, s"Round Tripped case class: ($roundTripped) did not equal the original: ($value)")
+  }
+
+  def encode[T](codec: Codec[T], value: T): OutputBuffer = {
+    val buffer = new BasicOutputBuffer()
+    val writer = new BsonBinaryWriter(buffer)
+    codec.encode(writer, value, EncoderContext.builder.build)
+    buffer
+  }
+
+  def decode[T](codec: Codec[T], buffer: OutputBuffer): T = {
+    val reader = new BsonBinaryReader(new ByteBufferBsonInput(new ByteBufNIO(ByteBuffer.wrap(buffer.toByteArray))))
+    codec.decode(reader, DecoderContext.builder().build())
+  }
+
+  def decode[T](value: T, json: String, codec: Codec[T]): Unit = {
+    val roundTripped = decode(codec, encode(documentCodec, Document(json)))
+    assert(roundTripped == value, s"Round Tripped case class: ($roundTripped) did not equal the original: ($value)")
+  }
+  
+  val documentCodec: Codec[Document] = DEFAULT_CODEC_REGISTRY.get(classOf[Document])
 }
