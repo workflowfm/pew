676b9ea9e653d1d5288fc99adb07e760d29d394d
jevaughan <j.e.vaughan@outlook.com>
2018-09-24 12:18:08 +0100

    Added kafka/zookeeper server scripts to resources/kafka directory

---
+++ resources/kafka/clean-topics.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+
+## DELETE ##
+
+$1/bin/kafka-topics.sh --zookeeper localhost:2181 --delete \
+            --topic Assignment
+
+$1/bin/kafka-topics.sh --zookeeper localhost:2181 --delete \
+            --topic Result
+
+$1/bin/kafka-topics.sh --zookeeper localhost:2181 --delete \
+            --topic PiiHistory
+
+$1/bin/kafka-topics.sh --zookeeper localhost:2181 --delete \
+            --topic ReduceRequest
+
+## RESTORE ##
+
+$1/bin/kafka-topics.sh --create --zookeeper localhost:2181 \
+            --replication-factor 1 --partitions 12 \
+            --topic Assignment
+
+$1/bin/kafka-topics.sh --create --zookeeper localhost:2181 \
+            --replication-factor 1 --partitions 4 \
+            --topic Result
+
+$1/bin/kafka-topics.sh --create --zookeeper localhost:2181 \
+            --replication-factor 1 --partitions 4 \
+            --topic PiiHistory
+
+$1/bin/kafka-topics.sh --create --zookeeper localhost:2181 \
+            --replication-factor 1 --partitions 4 \
+            --topic ReduceRequest
---
+++ resources/kafka/stop-servers.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+fuser 9092/tcp -k
+fuser 2181/tcp -k
