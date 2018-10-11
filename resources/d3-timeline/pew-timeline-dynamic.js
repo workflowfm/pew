c2a795dedcff205d85d491bf176c56b232eae1ae
Petros Papapanagiotou <petrospapapan@gmail.com>
2018-10-11 02:12:59 +0100

    Preliminary work on a dynamic timeline
    Notes:
    - We are currently updating by redrawing the whole svg. Can we update
    the data on the chart and transition? (see
    http://bl.ocks.org/d3noob/7030f35b72de721622b8)
    - Can we scroll to the end?
    - Can we keep the y axis frozen when we scroll?

---
+++ resources/d3-timeline/pew-timeline-dynamic.js
@@ -0,0 +1,78 @@
+var widthPerTick = 60
+
+function displayResults(tag,data) {
+
+	
+	var colorScale = d3.scale.category20().domain(tasks); 
+	//var ticks = Array(totalTicks).fill().map((v,i)=>i);
+	
+	var chart = d3.timeline()
+		.tickFormat( //
+				{format: d3.format("03d"),
+				tickInterval: 1, // This forces us to start from 001 
+				numTicks: totalTicks,
+				//tickValues: ticks, // Use this to start from 000
+				tickSize: 10,
+				})
+		/*.tickFormat( //
+				{format: d3.time.format("%H"),
+				tickTime: d3.time.hours,
+				tickInterval: 1,
+				tickSize: 10,
+				})*/
+		.stack()
+		.margin({left:100, right:30, top:0, bottom:0})
+		.colors( colorScale )
+		.colorProperty('task');
+	chart.showTimeAxisTick();
+	//chart.relativeTime();
+	//chart.rowSeparators("#555555");
+
+	var backgroundColor = "#eeeeee";
+	var altBackgroundColor = "white";
+	chart.background(function (datum, i) {
+		var odd = (i % 2) === 0;
+		return odd ? altBackgroundColor : backgroundColor;
+	});
+	chart.fullLengthBackgrounds();
+
+	var div = d3.select("body").append("div")	
+		.attr("class", "tooltip")				
+		.style("opacity", 0);
+	
+	chart.mouseover(function (d, i, datum) {
+		// d is the current rendering object
+		// i is the index during d3 rendering
+		// datum is the data object
+		div.style("left", (d3.event.pageX) + "px")		
+           .style("top", (d3.event.pageY - 28) + "px");
+		div.text(d.task + "\n" +
+				 chart.tickFormat().format(new Date(d.starting_time)) + "-" + chart.tickFormat().format(new Date(d.ending_time)) + "\n" +
+				 "Delay: " + chart.tickFormat().format(new Date(d.delay)) + "\n" +
+				 "Cost: " + d.cost
+		);
+		div.transition()		
+        	.duration(200)		
+        	.style("opacity", .9);		
+	});
+	chart.mouseout(function (d, i, datum) {
+		div.transition()		
+        	.duration(500)		
+        	.style("opacity", 0);	
+	});
+	
+	var svg = d3.select(tag).append("svg").attr("width", totalTicks*widthPerTick)
+		.datum(data).call(chart);
+}
+displayResults("#results",resourceData);
+
+var data = 0
+
+function updateData() {
+	var svg = d3.select("#results").select("svg").remove();
+	displayResults("#results",workflowData);
+
+    // Select the section we want to apply our changes to
+    //var bod = d3.select("#timeline").transition();
+
+}
