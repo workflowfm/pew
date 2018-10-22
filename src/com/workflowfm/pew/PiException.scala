1438fbd54b6b867eef53bfa6c5fd2c16855a9295
jevaughan <j.e.vaughan@outlook.com>
2018-10-22 11:30:02 +0100

    Fixing up PiExceptions

--- src/com/workflowfm/pew/PiException.scala
+++ src/com/workflowfm/pew/PiException.scala
@@ -1,9 +1,11 @@
 package com.workflowfm.pew
 
-sealed abstract class PiException[KeyT]
-  extends Exception {
+sealed abstract class PiException[KeyT]( message: String )
+  extends Exception( message ) {
 
   def id: KeyT
+
+  def event: PiExceptionEvent[KeyT]
 }
 
 sealed trait HasPiInstance[KeyT]
@@ -14,36 +16,53 @@ sealed trait HasPiInstance[KeyT]
 }
 
 case class NoResultException[KeyT]( override val pii: PiInstance[KeyT] )
-  extends PiException[KeyT] with HasPiInstance[KeyT]
+  extends PiException[KeyT]( s"[${pii.id}] NO RESULT!" )
+    with HasPiInstance[KeyT] {
 
-case class UnknownProcessException[KeyT]( pii: PiInstance[KeyT], process: String )
-  extends PiException[KeyT] with HasPiInstance[KeyT]
+  override def event: PiExceptionEvent[KeyT]
+    = PiFailureNoResult( pii )
+}
 
-case class AtomicProcessIsCompositeException[KeyT]( pii: PiInstance[KeyT], process: String )
-  extends PiException[KeyT] with HasPiInstance[KeyT]
+case class UnknownProcessException[KeyT]( override val pii: PiInstance[KeyT], process: String )
+  extends PiException[KeyT]( s"[${pii.id}] FAILED - Unknown process: $process" )
+    with HasPiInstance[KeyT] {
 
-case class NoSuchInstanceException[KeyT]( override val id: KeyT ) extends PiException[KeyT]
+  override def event: PiExceptionEvent[KeyT]
+    = PiFailureUnknownProcess( pii, process )
+}
+
+case class AtomicProcessIsCompositeException[KeyT]( override val pii: PiInstance[KeyT], process: String )
+  extends PiException[KeyT]( s"[${pii.id}] FAILED - Executor encountered composite process thread: $process" )
+    with HasPiInstance[KeyT] {
+
+  override def event: PiExceptionEvent[KeyT]
+    = PiFailureAtomicProcessIsComposite( pii, process )
+}
+
+case class NoSuchInstanceException[KeyT]( override val id: KeyT )
+  extends PiException[KeyT]( s"[$id] FAILED - Failed to find instance!" ) {
+
+  override def event: PiExceptionEvent[KeyT]
+   = PiFailureNoSuchInstance( id )
+}
 
 case class RemoteException[KeyT](
   override val id:  KeyT,
   message:          String,
   stackTrace:       String
-) extends PiException[KeyT]
+) extends PiException[KeyT]( message ) {
+
+  override def event: PiExceptionEvent[KeyT]
+    = PiEventException( id, message, stackTrace, System.nanoTime() )
+}
 
 case class RemoteProcessException[KeyT](
   override val id: KeyT,
   ref: Int,
   message: String,
   stackTrace: String
-) extends PiException[KeyT]
+) extends PiException[KeyT]( message ) {
 
-object PiException {
-  implicit def apply[KeyT]: PiException[KeyT] => PiExceptionEvent[KeyT] = {
-    case ex: NoResultException[KeyT]                  => PiFailureNoResult( ex.pii )
-    case ex: UnknownProcessException[KeyT]            => PiFailureUnknownProcess( ex.pii, ex.process )
-    case ex: AtomicProcessIsCompositeException[KeyT]  => PiFailureAtomicProcessIsComposite( ex.pii, ex.process )
-    case ex: NoSuchInstanceException[KeyT]            => PiFailureNoSuchInstance( ex.id )
-    case ex: RemoteException[KeyT]                    => PiEventException( ex.id, ex.message, ex.stackTrace, System.nanoTime() )
-    case ex: RemoteProcessException[KeyT]             => PiEventProcessException( ex.id, ex.ref, ex.message, ex.stackTrace, System.nanoTime() )
-  }
+  override def event: PiExceptionEvent[KeyT]
+    = PiEventProcessException( id, ref, message, stackTrace, System.nanoTime() )
 }
