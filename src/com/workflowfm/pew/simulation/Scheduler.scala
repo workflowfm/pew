ac5c78221d3a4f6d167a0d9d013910d6a478ca77
petrospapapa <pe.p@ed.ac.uk>
2018-11-19 17:31:36 +0000

    Updated Schedule to use tasks instead of gaps

--- src/com/workflowfm/pew/simulation/Scheduler.scala
+++ src/com/workflowfm/pew/simulation/Scheduler.scala
@@ -32,44 +32,97 @@ object DefaultScheduler extends Scheduler {
 
 }
 
-case class Schedule(gaps:List[(Long,Long)]) {
-  def +(start:Long,end:Long):Option[Schedule] = Schedule.fit(start,end,gaps) match {
+case class Schedule(tasks:List[(Long,Long)]) {
+  def +(start:Long,end:Long):Option[Schedule] = Schedule.add(start,end,tasks) match {
     case None => None
-    case Some(l) => Some(copy(gaps=l))
+    case Some(l) => Some(copy(tasks=l))
   }
 
   def +(startTime:Long, t:Task):Option[Schedule] = this + (startTime,startTime+t.estimatedDuration)
 
-  def ++(s:Schedule):Schedule = Schedule(Schedule.merge(gaps,s.gaps))
+  def ?(currentTime:Long, t:Task):(Long,Long) = {
+    val s = Schedule.fit(currentTime,t.estimatedDuration,tasks)
+    (s , s+t.estimatedDuration)
+  }
 
-  def isValid = Schedule.isValid(gaps)
+  def ++(s:Schedule):Schedule = Schedule(Schedule.merge(tasks,s.tasks))
+
+  def isValid = Schedule.isValid(tasks)
 }
 
 object Schedule {
   import scala.collection.immutable.Queue
 
-  def apply(r:TaskResource,currentTime:Long) = r.currentTask match {
-    case None => (currentTime,Long.MaxValue)
-    case Some((s,t)) => (s + t.estimatedDuration,Long.MaxValue)
+  def apply(r:TaskResource):Schedule = r.currentTask match {
+    case None => Schedule(List())
+    case Some((s,t)) => Schedule((s,s + t.estimatedDuration) :: Nil)
+  }
+
+  def add (
+    start:Long, end:Long,
+    tasks:List[(Long,Long)],
+    result:Queue[(Long,Long)] = Queue[(Long,Long)]()
+  ):Option[List[(Long,Long)]] = tasks match {
+    case Nil => Some(result :+ (start,end) toList)
+    case (l:Long,r:Long) :: t =>
+      if (l > end) Some(result ++ ((start,end) :: (l,r) :: t) toList)
+      else if (l == end) Some(result ++ ((start,r) :: t) toList)
+      else if (r < start) add(start,end,t,result :+ ((l,r)))
+      else if (r == start) add(l,end,t,result)
+      else /* if (r >= end) */ None
+      //else None
   }
 
   def fit (
+    start:Long,
+    duration:Long,
+    tasks:List[(Long,Long)]
+  ):Long = tasks match {
+    case Nil => start
+    case (l,_) :: _ if (l >= start + duration) => start
+    case (_,r) :: t => fit(r,duration,t)
+  }
+
+  // we assume all gap lists finish with a (t,Long.MaxValue) gap
+  def merge(
+    g1:List[(Long,Long)],
+    g2:List[(Long,Long)],
+    result:Queue[(Long,Long)] = Queue[(Long,Long)]()
+  ):List[(Long,Long)] = g1 match {
+    case Nil => result ++ g2 toList
+    case (l1,r1) :: t1 => g2 match {
+      case Nil => result ++ g1 toList
+      case (l2,r2) :: t2 => {
+        if (r2 < l1) merge(g1,t2,result :+ (l2,r2))
+        else if (r1 < l2) merge(t1,g2,result :+ (l1,r1))
+        else if (r1 == r2) merge(t1,t2,result :+ (math.min(l1,l2),r1))
+        else if (r2 == l1) merge((l2,r1)::t1,t2,result)
+        else if (r1 == l2) merge(t1,(l1,r2)::t2,result)
+        else if (r1 < r2) merge(t1,(math.min(l1,l2),r2)::t2,result)
+        else /* if (r1 > r2)*/ merge((math.min(l1,l2),r1)::t1,t2,result)
+      }
+    }
+  }
+
+  @deprecated("No longer using gaps in Schedule","1.2.0")
+  def fitInGaps (
     start:Long, end:Long,
     gaps:List[(Long,Long)],
     result:Queue[(Long,Long)] = Queue[(Long,Long)]()
   ):Option[List[(Long,Long)]] = gaps match {
     case Nil => Some(result.toList)
     case (l:Long,r:Long) :: t =>
-      if (l == start && end == r) fit(start,end,t,result) // event fits exactly
-      else if (l == start && end <= r) fit(start,end,t,result :+ ((end,r)) )// add an event at the beginning of the gap
-      else if (l <= start && end == r) fit(start,end,t,result :+ ((l,start)) ) // add an event at the end of the gaps
-      else if (l < start && end < r) fit(start,end,t,result :+ ((l,start)) :+ ((end,r)) ) // add an event within a gap
-      else if (start > r || end < l) fit(start,end,t,result :+ ((l,r)) )
+      if (l == start && end == r) fitInGaps(start,end,t,result) // event fits exactly
+      else if (l == start && end <= r) fitInGaps(start,end,t,result :+ ((end,r)) )// add an event at the beginning of the gap
+      else if (l <= start && end == r) fitInGaps(start,end,t,result :+ ((l,start)) ) // add an event at the end of the gaps
+      else if (l < start && end < r) fitInGaps(start,end,t,result :+ ((l,start)) :+ ((end,r)) ) // add an event within a gap
+      else if (start > r || end < l) fitInGaps(start,end,t,result :+ ((l,r)) )
       else None
   }
 
+  @deprecated("No longer using gaps in Schedule","1.2.0")
   // we assume all gap lists finish with a (t,Long.MaxValue) gap
-  def merge (
+  def mergeGaps (
     g1:List[(Long,Long)],
     g2:List[(Long,Long)],
     result:Queue[(Long,Long)] = Queue[(Long,Long)]()
@@ -78,18 +131,18 @@ object Schedule {
     case (l1,r1) :: t1 => g2 match {
       case Nil => result toList
       case (l2,r2) :: t2 => {
-        if (r2 <= l1) merge(g1,t2,result)
-        else if (r1 <= l2) merge (t1,g2,result)
+        if (r2 <= l1) mergeGaps(g1,t2,result)
+        else if (r1 <= l2) mergeGaps (t1,g2,result)
         else if (r1 == Long.MaxValue && r1 == r2) result :+ (math.max(l1,l2),r1) toList
-        else if (r2 <= r1) merge(g1,t2,result :+ (math.max(l1,l2),r2))
-        else /* if (r1 < r2) */ merge(t1,g2,result :+ (math.max(l1,l2),r1))
+        else if (r2 <= r1) mergeGaps(g1,t2,result :+ (math.max(l1,l2),r2))
+        else /* if (r1 < r2) */ mergeGaps(t1,g2,result :+ (math.max(l1,l2),r1))
       }
     }
   }
 
-  def isValid(gaps:List[(Long,Long)], start:Long = Long.MinValue):Boolean = gaps match {
+  def isValid(gaps:List[(Long,Long)], end:Long = Long.MinValue):Boolean = gaps match {
     case Nil => true
-    case (l,r) :: t if start < l && l < r => isValid(t, r)
+    case (l,r) :: t if end < l && l < r => isValid(t, r)
     case _ => false
   }
 }
--- test/com/workflowfm/pew/simulation/Scheduler.scala
+++ test/com/workflowfm/pew/simulation/Scheduler.scala
@@ -10,38 +10,90 @@ class SchedulerTests extends WordSpecLike with Matchers with BeforeAndAfterAll
 
   "The Schedule" must {
 
-    "fit a task exactly" in {
-      s((1,3)) + (1,3) should be (Some(s()))
-      s((1,3),(4,5)) + (1,3) should be (Some(s((4,5))))
-      s((1,3),(4,5)) + (4,5) should be (Some(s((1,3))))
+    "fit a task at the edge of another" in {
+      s((1,2)) + (2,3)  should be (Some(s((1,3))))
+      s((3,4)) + (2,3)  should be (Some(s((2,4))))
     }
 
-    "fit a task at the start" in {
-      s((1,3)) + (1,2) should be (Some(s((2,3))))
-      s((1,3),(4,5)) + (1,2) should be (Some(s((2,3),(4,5))))
-      s((1,3),(4,6)) + (4,5) should be (Some(s((1,3),(5,6))))
+    "fit a task at the edge of two others" in {
+      s((1,2),(3,4)) + (2,3)  should be (Some(s((1,4))))
+      s((1,2),(4,5)) + (2,3)  should be (Some(s((1,3),(4,5))))
+      s((1,2),(4,5)) + (3,4)  should be (Some(s((1,2),(3,5))))
     }
 
-    "fit a task at the end" in {
-      s((1,3)) + (2,3) should be (Some(s((1,2))))
-      s((1,3),(4,5)) + (2,3) should be (Some(s((1,2),(4,5))))
-      s((1,3),(4,6)) + (5,6) should be (Some(s((1,3),(4,5))))
+    "fit a task in gaps" in {
+      s((1,2)) + (3,4) should be (Some(s((1,2),(3,4))))
+      s((3,4)) + (1,2) should be (Some(s((1,2),(3,4))))
+      s((1,2),(3,4)) + (5,6) should be (Some(s((1,2),(3,4),(5,6))))
+      s((1,2),(5,6)) + (3,4) should be (Some(s((1,2),(3,4),(5,6))))
+      s((3,4),(5,6)) + (1,2) should be (Some(s((1,2),(3,4),(5,6))))
     }
 
-    "fit a task in-between" in {
-      s((1,4)) + (2,3) should be (Some(s((1,2),(3,4))))
-      s((1,4),(5,6)) + (2,3) should be (Some(s((1,2),(3,4),(5,6))))
-      s((1,4),(5,8)) + (6,7) should be (Some(s((1,4),(5,6),(7,8))))
-    }
-
-    "not fit tasks that don't fit" in {
+    "not fit tasks that clash with the start of another task" in {
       s((1,2)) + (1,3) should be (None)
-      s((1,2),(3,4)) + (1,3) should be (None)
-      s((1,2),(3,4)) + (3,5) should be (None)
+      s((1,4)) + (1,3) should be (None)
       s((2,3)) + (1,3) should be (None)
-      s((2,3),(4,5)) + (1,3) should be (None)
-      s((2,3),(4,5)) + (3,5) should be (None)
+      s((2,4)) + (1,3) should be (None)
+    }
+
+    "not fit tasks that clash with the end of another task" in {
+      s((2,4)) + (3,4) should be (None)
+      s((3,4)) + (2,4) should be (None)
+      s((1,4)) + (1,5) should be (None)
+      s((1,5)) + (1,4) should be (None)
+      s((2,3)) + (1,3) should be (None)     
+    }
+
+    "not fit tasks that overlap with another task" in {
+      s((1,3)) + (1,3) should be (None)
+      s((1,4)) + (2,3) should be (None)
       s((2,3)) + (1,4) should be (None)
+    }
+
+    "not fit tasks that clash with two other tasks" in {
+      s((1,2),(4,6)) + (2,5) should be (None)
+      s((1,2),(4,6)) + (3,5) should be (None)
+      s((1,2),(4,6)) + (2,6) should be (None)
+      s((1,2),(4,6)) + (3,6) should be (None)
+      s((1,2),(4,6)) + (2,7) should be (None)
+      s((1,2),(4,6)) + (3,7) should be (None)
+      s((1,3),(4,6)) + (2,4) should be (None)
+    }
+
+    "merge single tasks with common start" in {
+      s((1,2)) ++ s((1,2)) should be (s((1,2)))
+      s((1,2)) ++ s((1,3)) should be (s((1,3)))
+      s((1,3)) ++ s((1,2)) should be (s((1,3)))
+    }
+
+    "merge single tasks with common finish" in {
+      s((1,3)) ++ s((2,3)) should be (s((1,3)))
+      s((2,3)) ++ s((1,3)) should be (s((1,3)))
+    }
+
+    "merge single tasks that don't overlap" in {
+      s((1,2)) ++ s((2,3)) should be (s((1,3)))
+      s((1,2)) ++ s((3,4)) should be (s((1,2),(3,4)))
+      s((1,2),(5,6)) ++ s((3,4)) should be (s((1,2),(3,4),(5,6)))
+      s((2,3)) ++ s((1,2)) should be (s((1,3)))
+      s((3,4)) ++ s((1,2)) should be (s((1,2),(3,4)))
+      s((3,4)) ++ s((1,2),(5,6)) should be (s((1,2),(3,4),(5,6)))
+    }
+
+    "merge multiple tasks with one overlapping task" in {
+      s((1,2),(3,4),(5,6)) ++ s((1,6)) should be (s((1,6)))
+      s((1,2),(3,4),(5,6)) ++ s((0,6)) should be (s((0,6)))
+      s((1,2),(3,4),(5,6)) ++ s((1,7)) should be (s((1,7)))
+      s((1,2),(3,4),(5,6)) ++ s((0,7)) should be (s((0,7)))
+      s((1,6)) ++ s((1,2),(3,4),(5,6)) should be (s((1,6)))
+      s((0,6)) ++ s((1,2),(3,4),(5,6)) should be (s((0,6)))
+      s((1,7)) ++ s((1,2),(3,4),(5,6)) should be (s((1,7)))
+      s((0,7)) ++ s((1,2),(3,4),(5,6)) should be (s((0,7)))
+    }
+
+    "merge multiple overlapping tasks" in {
+      s((1,2),(3,4),(5,6)) ++ s((2,3),(4,5),(6,7)) should be (s((1,7)))
+      s((1,2),(3,4),(5,6)) ++ s((2,3),(4,5)) should be (s((1,6)))
     }
   }
 
