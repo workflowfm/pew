15765148b3a9a888598239a2e1a5dce80ced6a32
Petros Papapanagiotou <petrospapapan@gmail.com>
2019-07-17 17:50:31 +0100

    Sets up pew simulator environment

--- simulator/src/main/scala/com/workflowfm/pew/simulator/PiSimulation.scala
+++ simulator/src/main/scala/com/workflowfm/pew/simulator/PiSimulation.scala
@@ -1,4 +1,22 @@
 package com.workflowfm.pew.simulator
 
-class PiSimulation {
+import com.workflowfm.pew.{ AtomicProcess, PiProcess }
+import com.workflowfm.pew.execution.SimulatorExecutor
+import com.workflowfm.simulator.{ SimulatedProcess, Simulation }
+import scala.concurrent.Future
+
+trait SimulatedPiProcess extends AtomicProcess with SimulatedProcess {
+  override def isSimulatedProcess = true
 }
+
+abstract class PiSimulation (val name: String) {
+  def run(executor: SimulatorExecutor[_]): Future[Any]
+  def getProcesses(): Seq[PiProcess]
+  def simulateWith(executor: SimulatorExecutor[_]) :Simulation = new PiSimulationInstance(this, executor)
+}
+
+class PiSimulationInstance(sim: PiSimulation, executor: SimulatorExecutor[_]) extends Simulation(sim.name) {
+  override def run(): Future[Any] = sim.run(executor)
+  override def simulationReady = executor.simulationReady
+}
+
