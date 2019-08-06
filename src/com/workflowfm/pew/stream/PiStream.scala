1815b91c619a474f699e976f8657926e979d1e9a
Petros Papapanagiotou <petrospapapan@gmail.com>
2019-08-06 11:06:32 +0100

    Uses SubAkka.HashSetPublisher and SubscriptionSwitch

--- src/com/workflowfm/pew/stream/PiStream.scala
+++ src/com/workflowfm/pew/stream/PiStream.scala
@@ -5,20 +5,20 @@ import akka.stream.KillSwitch
 import com.workflowfm.pew.{ PiEvent, PiEventResult }
 import scala.concurrent.{ ExecutionContext, Future }
 import scala.reflect.ClassTag
-import uk.ac.ed.inf.ppapapan.subakka.{ Publisher, Subscriber }
+import uk.ac.ed.inf.ppapapan.subakka.{ HashSetPublisher, Subscriber, SubscriptionSwitch }
 
 class PiSubscriber[T](handler: PiEventHandler[T]) extends Subscriber[PiEvent[T]] {
-  var switch: Option[KillSwitch] = None
+  var switch: Option[SubscriptionSwitch] = None
 
-  override def onInit(publisher: ActorRef, k: KillSwitch): Unit = switch = Some(k)
-  override def onEvent(event: PiEvent[T]): Unit = if (handler(event)) switch.map(_.shutdown())
+  override def onInit(publisher: ActorRef, killSwitch: SubscriptionSwitch): Unit = switch = Some(killSwitch)
+  override def onEvent(event: PiEvent[T]): Unit = if (handler(event)) switch.map(_.stop())
 }
 
 /** A [[PiSource]] that also implements [[PiPublisher]] with Akka Streams.
   * Uses [[akka.stream.scaladsl.BroadcastHub]], which allows us to clone new sources and attach the
   * handlers as sinks.
   */
-trait PiStream[T] extends PiPublisher[T] with Publisher[PiEvent[T]] with PiObservable[T] {
+trait PiStream[T] extends PiPublisher[T] with HashSetPublisher[PiEvent[T]] with PiObservable[T] {
   implicit val tag: ClassTag[PiEvent[T]]
   implicit val executionContext: ExecutionContext
 
@@ -29,6 +29,6 @@ trait PiStream[T] extends PiPublisher[T] with Publisher[PiEvent[T]] with PiObser
 
 
 /** A wrapper of [[akka.stream.KillSwitch]] to stop [[PiEventHandler]]s. */
-case class PiKillSwitch(switch: KillSwitch) extends PiSwitch {
-  override def stop = switch.shutdown()
+case class PiKillSwitch(switch: SubscriptionSwitch) extends PiSwitch {
+  override def stop = switch.stop()
 }
