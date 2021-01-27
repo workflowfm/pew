96fe4a7c7e1e9d6007db22638528868c6f9134ae
Petros Papapanagiotou <petrospapapan@gmail.com>
2021-01-27 17:53:52 +0000

    Adds new `PiSimulation` and `PiSimulatedProcess`

--- pew-simulator/src/main/scala/com/workflowfm/pew/simulator/PiSimulatedProcess.scala
+++ pew-simulator/src/main/scala/com/workflowfm/pew/simulator/PiSimulatedProcess.scala
@@ -13,30 +13,34 @@ import akka.util.Timeout
 import com.workflowfm.pew.{ AtomicProcess, PiProcess }
 import com.workflowfm.pew.{ MetadataAtomicProcess, PiInstance, PiMetadata, PiObject }
 import com.workflowfm.pew.stream.{ PiEventHandler, PiEventHandlerFactory }
-import com.workflowfm.simulator.{ SimulatedProcess, Task, TaskGenerator }
+import com.workflowfm.pew.simulator.PiSimulation
+import com.workflowfm.proter.{ Task, TaskInstance }
 
-trait PiSimulatedProcess extends AtomicProcess with SimulatedProcess {
-  override val iname: String = s"$simulationName.$name"
 
-  /* TODO We never actually wait for these asks, so there is still a chance the ordering will be
-   * messed up */
-  // if the messages are delayed
-  override def simWait(): Unit =
-    (simulationActor ? PiSimulation.Waiting(iname))(Timeout(1, TimeUnit.DAYS))
+trait PiSimulatedProcess extends AtomicProcess {
+  def simulation: PiSimulation
 
-  def simResume(): Future[Any] =
-    (simulationActor ? PiSimulation.Resuming(iname))(Timeout(1, TimeUnit.DAYS))
+  def simulate[T](
+    gen: Task,
+    resultFun: (TaskInstance, Long) => T
+  ): Future[T] = {
+    simulation.simulate(iname, gen, resultFun)
+  }
 
-  override def simulate[T](
-      gen: TaskGenerator,
-      result: (Task, Long) => T,
-      resources: String*
-  )(implicit executionContext: ExecutionContext): Future[T] = {
-    val f = (simulationActor ? PiSimulation.AddTask(iname, gen, resources))(
-      Timeout(1, TimeUnit.DAYS)
-    ).mapTo[(Task, Long)].map { case (task, time) => result(task, time) }
-    simWait()
-    f
+  def simulate[T](
+    iname: String,
+    gen: Task,
+    result: T
+  ): Future[T] = { 
+    simulation.simulate(iname, gen, result)
+  }
+
+  def waiting(): Unit = {
+    simulation.processWaiting(iname)
+  }
+
+  def resume(): Unit = {
+    simulation.processResuming(iname)
   }
 
 }
